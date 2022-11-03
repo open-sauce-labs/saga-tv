15231cc839ef62300a56cf187e42e2718a24d8f9
josip-volarevic <josip.volarevic4@gmail.com>
2022-11-03 09:49:54 +0100

    chore: update theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ class MyDocument extends Document {
 
 					<link rel='manifest' href='/manifest.json' />
 					<link rel='apple-touch-icon' href='/apple-touch-icon.png'></link>
-					<meta name='theme-color' content='#d7ae6e' />
+					<meta name='theme-color' content='#000' />
 				</Head>
 				<body>
 					<Main />
